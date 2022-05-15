8fb29c0aa7d33b2903024b37d21c9435e1140be6
DaanGamesDG <daan@daangamesdg.xyz>
2022-05-15 13:56:13 +0200

    feat(Automod): add duplicate text checker

--- src/client/lib/structures/Moderation/AutoMod.ts
+++ src/client/lib/structures/Moderation/AutoMod.ts
@@ -1,9 +1,11 @@
-import type { Invite } from "discord.js";
+import { Collection, Invite } from "discord.js";
 import type { Client } from "../../../client";
 import { INVITE_REGEX } from "./regex";
-import type { AutoModResults, GuildMessage } from "./types";
+import type { AutoModDupCache, AutoModResults, GuildMessage } from "./types";
 
 export class AutoMod {
+	public dupTextCache: AutoModDupCache = new Collection();
+
 	public constructor(public client: Client) {}
 
 	public async invite(message: GuildMessage): Promise<AutoModResults | null> {
@@ -28,5 +30,38 @@ export class AutoMod {
 				target: invite.guild?.name ?? invite.channel.name
 			}
 		};
+	}
+
+	public dupText(message: GuildMessage): AutoModResults | null {
+		const newContent = message.content.toLowerCase();
+		const dupId = `${message.author.id}-${message.guildId}`;
+
+		const dupData = this.dupTextCache.get(dupId);
+		if (dupData) {
+			if (dupData.lastMessage === newContent)
+				return {
+					guild: message.guildId,
+					user: message.author.id,
+					date: Date.now(),
+					key: "AUTOMOD_DUP_TEXT"
+				};
+
+			clearTimeout(dupData.timeout);
+			const timeout = setTimeout(() => this.dupTextCache.delete(dupId), 6e4);
+			this.dupTextCache.set(dupId, {
+				...dupData,
+				timeout
+			});
+		} else {
+			const timeout = setTimeout(() => this.dupTextCache.delete(dupId), 6e4);
+			this.dupTextCache.set(dupId, {
+				guildId: message.guildId,
+				userId: message.author.id,
+				lastMessage: newContent,
+				timeout
+			});
+		}
+
+		return null;
 	}
 }
--- src/client/lib/structures/Moderation/types.ts
+++ src/client/lib/structures/Moderation/types.ts
@@ -1,4 +1,4 @@
-import type { Guild, GuildChannel, GuildMember, Message } from "discord.js";
+import type { Collection, Guild, GuildChannel, GuildMember, Message } from "discord.js";
 
 export type GuildMessage = Message & {
 	guild: Guild;
@@ -12,5 +12,17 @@ export interface AutoModResults {
 	user: string;
 	date: number;
 	key: string;
-	vars: Record<string, any>;
+	vars?: Record<string, any>;
 }
+
+export interface AutoModCacheDupData {
+	userId: string;
+	guildId: string;
+	lastMessage: string;
+	timeout: NodeJS.Timeout;
+}
+
+/**
+ * Key for the object is **userId-guildId**
+ */
+export type AutoModDupCache = Collection<string, AutoModCacheDupData>;
