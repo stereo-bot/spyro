a77410fd10534526756a15c676b23a4d95bfa7c1
DaanGamesDG <daan@daangamesdg.xyz>
2022-05-15 13:28:29 +0200

    feat: move all regex to regex file

--- src/client/lib/structures/Moderation/AutoMod.ts
+++ src/client/lib/structures/Moderation/AutoMod.ts
@@ -1,13 +1,13 @@
 import type { Invite } from "discord.js";
 import type { Client } from "../../../client";
+import { INVITE_REGEX } from "./regex";
 import type { AutoModResults, GuildMessage } from "./types";
 
 export class AutoMod {
 	public constructor(public client: Client) {}
 
 	public async invite(message: GuildMessage): Promise<AutoModResults | null> {
-		const regex = /discord(?:(?:app)?\.com\/invite|\.gg(?:\/invite)?)\/([\w-]{2,255})/gi;
-		const invites = message.content.match(regex) ?? [];
+		const invites = message.content.match(INVITE_REGEX) ?? [];
 
 		let invite: Invite | null = null;
 		for await (const inviteLink of invites) {
---
+++ src/client/lib/structures/Moderation/regex.ts
@@ -0,0 +1,2 @@
+export const EMOJI_REGEX = /(<a?)?:\w+:(\d{18}>)?/g;
+export const INVITE_REGEX = /discord(?:(?:app)?\.com\/invite|\.gg(?:\/invite)?)\/([\w-]{2,255})/gi;
